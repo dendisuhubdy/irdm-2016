b38b3ac741a47ebb7b11b40efbc521bb8938d416
Helias <helias1990@hotmail.com>
2016-04-08 00:18:48 +0100

    script for saving images tested

--- src/save_images.py
+++ src/save_images.py
@@ -12,20 +12,22 @@ data = loadDataset('../data/catalog.xls', starting_year=1600, ordered=False)
 # begin timer in order to see how long it takes to save images
 start = time.time()
 
-# list_file = open('../listimages.txt', 'w')
+list_file = open('../listimages.txt', 'w')
 
 # Process all images -- Create Image objects and add to the dictionary
 for index, row in data.iterrows():
 
     painting = Painting(index, int(row.YEAR), row.AUTHOR, row.URL, row.SCHOOL)
-    painting.save_painting()
 
-    print(str(index+1)+'th image saved...')
+    print(str(index+1)+' '+painting.get_name()+' '+painting.get_url())
+    painting.save_painting(file=list_file)
 
-    if index + 1 == 50:
-        break
+    # print(str(index+1)+'th image saved...')
 
-# list_file.close()
+    # if index + 1 == 50:
+    #     break
+
+list_file.close()
 
 end = time.time()
 print 'Time to save images: '+str(end - start)
