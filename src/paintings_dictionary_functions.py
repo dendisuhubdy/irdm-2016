fbea05a29780f49bce61aa570aa444632d5d0fdd
Helias <helias1990@hotmail.com>
2016-04-09 00:45:30 +0100

    commented unused method in paintings_dictionary_functions

--- src/paintings_dictionary_functions.py
+++ src/paintings_dictionary_functions.py
@@ -4,30 +4,30 @@ import pandas as pd
 __author__ = 'helias'
 
 
-def get_max_min_pairs(paintings):
-    """
-    get_max_min_pair: finds the most similar and least similar of paintings over a collection of them
-    :param paintings: dictionary from any key (here an integer) to a Painting object
-    :return: max_similarity, pair of keys that correspond to most similar paintings (key1, key2),
-    min_similarity, pair of keys that correspond to least similar paintings as tuple (key3, key4)
-    """
-    max_pair = (0, 0)
-    min_pair = (0, 0)
-    max_sim = 0.0
-    min_sim = 1.0
-    for key in paintings.keys():
-        most_similar = paintings.get(key).get_max_similarity()
-        least_similar = paintings.get(key).get_min_similarity()
-
-        sim_most = paintings.get(key).get_similarity(most_similar)
-        sim_least = paintings.get(key).get_similarity(least_similar)
-
-        if sim_most > max_sim:
-            max_sim, max_pair = sim_most, (key, most_similar)
-        if sim_least < min_sim:
-            min_sim, min_pair = sim_least, (key, least_similar)
-
-    return max_sim, max_pair, min_sim, min_pair
+# def get_max_min_pairs(paintings):
+#     """
+#     get_max_min_pair: finds the most similar and least similar of paintings over a collection of them
+#     :param paintings: dictionary from any key (here an integer) to a Painting object
+#     :return: max_similarity, pair of keys that correspond to most similar paintings (key1, key2),
+#     min_similarity, pair of keys that correspond to least similar paintings as tuple (key3, key4)
+#     """
+#     max_pair = (0, 0)
+#     min_pair = (0, 0)
+#     max_sim = 0.0
+#     min_sim = 1.0
+#     for key in paintings.keys():
+#         most_similar = paintings.get(key).get_max_similarity()
+#         least_similar = paintings.get(key).get_min_similarity()
+#
+#         sim_most = paintings.get(key).get_similarity(most_similar)
+#         sim_least = paintings.get(key).get_similarity(least_similar)
+#
+#         if sim_most > max_sim:
+#             max_sim, max_pair = sim_most, (key, most_similar)
+#         if sim_least < min_sim:
+#             min_sim, min_pair = sim_least, (key, least_similar)
+#
+#     return max_sim, max_pair, min_sim, min_pair
 
 
 def set_innovations(paintings, years, years_back=5, top_paintings=5):
